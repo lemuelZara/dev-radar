d8f015a9d2ecd6a8098c8039baa4244729b732c3
lemuelZara <lemuel_zara@outlook.com>
2020-04-08 16:25:36 -0300

    creating the div aside

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import './App.css'
+import './Sidebar.css'
+import './Main.css'
+import './global.css'
 
 function App() {
   return (
-    <h1>DevRadar</h1>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Latitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+
+      </main>
+    </div>
   );
 }
 
