2471282d51d0095cd58e46fa06280a9ed7f7c306
lemuelZara <lemuel_zara@outlook.com>
2020-04-09 10:53:41 -0300

    comments on react

--- web/src/index.js
+++ web/src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
+// Componente  : Bloco isolado de HTML, CSS, JS
+// Estado      : Armazena informações de um componente
+// Propriedade : Informações que um Componente PAI passa para o Componente FILHO
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
