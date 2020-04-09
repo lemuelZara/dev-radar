5f5bff79acbadea6d96ee750dc2f9ac89186cb95
lemuelZara <lemuel_zara@outlook.com>
2020-04-09 10:30:25 -0300

    adding DevItem component

---
+++ web/src/components/DevItem/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const DevItem = ({ dev }) => {
+    return (
+        <li className="dev-item">
+            <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                    <strong>{dev.name} </strong>
+                    <span>{dev.techs.join(', ')}</span>
+                </div>
+            </header>
+            <p>{dev.bio}</p>
+            <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no GitHub</a>
+        </li>
+    )
+}
