0a2706953494456934da64134000a117c139c2ce
lemuelZara <lemuel_zara@outlook.com>
2020-04-10 13:17:05 -0300

    showing Dev's GitHub

--- mobile/src/pages/Profile.js
+++ mobile/src/pages/Profile.js
@@ -1,6 +1,13 @@
 import React from 'react'
-import { View } from 'react-native'
+import { WebView } from 'react-native-webview'
+import { useRoute } from '@react-navigation/native'
 
 export const Profile = () => {
-    return <View />
+    const route = useRoute()
+
+    const { github_username } = route.params
+
+    return <WebView
+        style={{ flex: 1 }}
+        source={{ uri: `https://github.com/${github_username}` }} />
 }
