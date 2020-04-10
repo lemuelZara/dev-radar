70d57e7d9a80766493a9af97870aab81492d6878
lemuelZara <lemuel_zara@outlook.com>
2020-04-10 14:04:02 -0300

    creating button to search for Devs

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image, TextInput, TouchableOpacity } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps'
 import { useNavigation } from '@react-navigation/native'
 import * as Location from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons'
 
 export const Main = () => {
     const [currentRegion, setCurrentRegion] = useState(null)
@@ -10,7 +11,7 @@ export const Main = () => {
     const navigation = useNavigation()
 
     const navigateToProfile = () => {
-        navigation.navigate('Profile', { github_username: 'lemuelZara'})
+        navigation.navigate('Profile', { github_username: 'lemuelZara' })
     }
 
     useEffect(() => {
@@ -41,30 +42,47 @@ export const Main = () => {
     }
 
     return (
-        <MapView style={styles.mapStyle} initialRegion={currentRegion}>
-            <Marker coordinate={{ latitude: -20.354880, longitude: -50.184209 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/55895642?s=460&u=23d04221f1435a5fcd494b20ebdeca20ce24b46a&v=4' }} />
+        <>
+            <MapView style={styles.mapStyle} initialRegion={currentRegion}>
+                <Marker coordinate={{ latitude: -20.354880, longitude: -50.184209 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/55895642?s=460&u=23d04221f1435a5fcd494b20ebdeca20ce24b46a&v=4' }} />
 
-                <Callout onPress={() => {
-                    navigateToProfile()
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Lemuel Coelho Zara</Text>
-                        <Text style={styles.devBio}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi hic nam voluptates vitae, reiciendis ducimus voluptatum? Qui numquam accusamus odio atque, illum incidunt. Ratione fugiat facilis rem magni culpa a!</Text>
-                        <Text style={styles.devTechs}>Node.js, Flutter</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+                    <Callout onPress={() => {
+                        navigateToProfile()
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Lemuel Coelho Zara</Text>
+                            <Text style={styles.devBio}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi hic nam voluptates vitae, reiciendis ducimus voluptatum? Qui numquam accusamus odio atque, illum incidunt. Ratione fugiat facilis rem magni culpa a!</Text>
+                            <Text style={styles.devTechs}>Node.js, Flutter</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity style={styles.loadButton} onPress={() => { }}>
+                    <MaterialIcons name="my-location" size={25} color="#fff" />
+                </TouchableOpacity>
+            </View>
+        </>
+
     )
 }
 
 const styles = StyleSheet.create({
     mapStyle: {
+        flex: 1,
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
     },
-
+ 
     avatar: {
         width: 54,
         height: 54,
@@ -93,5 +111,46 @@ const styles = StyleSheet.create({
     devTechs: {
         marginTop: 5,
         textAlign: 'center'
+    },
+
+    searchForm: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row'
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#fff',
+        color: '#666',
+        borderRadius: 10,
+        paddingHorizontal: 20,
+        fontSize: 16,
+
+        // Style from iOS
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+
+        // Style from Android
+        elevation: 1.5
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#7159c1',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
+        marginLeft: 15
+
     }
 });
